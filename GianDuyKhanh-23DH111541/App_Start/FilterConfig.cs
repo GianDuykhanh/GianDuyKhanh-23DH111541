7b658708594803f02f655502566f60e27016e32d
duykh <duykh@DUYKHANH>
2024-11-01 11:29:46 +0700

    Add project files.

---
+++ GianDuyKhanh-23DH111541/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GianDuyKhanh_23DH111541
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ GianDuyKhanh-23DH111541/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GianDuyKhanh_23DH111541.MvcApplication" Language="C#" %>
